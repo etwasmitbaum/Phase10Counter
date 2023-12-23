eac35c29cfe77be64a284932227470bb0700fc33
fz72 <fz72@gmx.de>
2023-12-23 14:16:56 +0100

    remove unused import

--- app/src/main/java/com/tjEnterprises/phase10Counter/ui/selectGame/SelectGameScreen.kt
+++ app/src/main/java/com/tjEnterprises/phase10Counter/ui/selectGame/SelectGameScreen.kt
@@ -1,20 +1,14 @@
 package com.tjEnterprises.phase10Counter.ui.selectGame
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tjEnterprises.phase10Counter.data.local.database.Game
 import com.tjEnterprises.phase10Counter.data.local.database.Player
