e4c2086b5ee1641d047f5f4044a30bb1b6d1eb53
etwasmitbaum <47484288+etwasmitbaum@users.noreply.github.com>
2025-01-27 20:04:28 +0100

    add strings for game type names

--- app/src/main/java/com/tjEnterprises/phase10Counter/data/local/models/GameType.kt
+++ app/src/main/java/com/tjEnterprises/phase10Counter/data/local/models/GameType.kt
@@ -14,17 +14,17 @@ object GameType {
 
     data object Standard : Type {
         override val key: String = DEFAULT_GAMETYPE_KEY
-        override val resourceId: Int = R.string.gameName
+        override val resourceId: Int = R.string.gameTypeStandard
     }
 
     data object Flip : Type {
         override val key = "flip"
-        override val resourceId: Int = R.string.gameName
+        override val resourceId: Int = R.string.gameTypeFlip
     }
 
     data object Masters : Type {
         override val key = "masters"
-        override val resourceId: Int = R.string.gameName
+        override val resourceId: Int = R.string.gameTypeMasters
     }
 
 }
