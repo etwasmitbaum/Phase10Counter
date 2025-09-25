f808e63ae311f0222336e32f45ac858ff5108675
etwasmitbaum <47484288+etwasmitbaum@users.noreply.github.com>
2025-09-26 00:01:22 +0200

    fix: localeFilter warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,10 +51,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-
-        androidResources {
-            localeFilters += arrayOf("en", "de", "pl")
-        }
     }
 
     buildTypes {
